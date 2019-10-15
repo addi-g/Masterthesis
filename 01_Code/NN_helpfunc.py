e754f67db51c57e323246ab3eae6f211eabea314
addi-g <56253562+addi-g@users.noreply.github.com>
2019-10-15 12:09:51 +0200

    Implementierung der NN Hilfsfunktionen

---
+++ 01_Code/NN_helpfunc.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 15 11:22:02 2019
+
+@author: adrian
+
+Implementation of neural networks that we need to contruct our neural network 
+regression estimate
+"""
+import numpy as np
+
+def sigmoid (x):
+    
+    return 1 / (1 + np.exp(-x))
+
+# Neuronales Netz welches die Funktion f(x) = x approximiert
+#
+# x: reelle Zahl
+# R: reelle Zahl >= 1
+    
+def f_id (x,R):
+    
+    return 4 * R * sigmoid(x / R) - 2 * R 
+
+# Neuronales Netz welches die Funktion f(x,y) = x*y approximiert
+#
+# x: reelle Zahl
+# y: reelle Zahl    
+# R: reelle Zahl >= 1
+    
+def f_mult (x,y,R):
+    
+    return ((R ** 2) / 4) * (((1 + np.exp(-1)) ** 3) / (np.exp(-2) - np.exp(-1))) \
+    * (sigmoid(((2 * (x + y)) / R) + 1) - 2 * sigmoid(((x + y) / R) + 1) \
+    - sigmoid(((2 * (x + y)) / R) + 1) + 2 * sigmoid(((x - y) / R) + 1)) 
+    
+# Neuronales Netz welches die Funktion f(x) = max(x,0) approximiert
+#
+# x: reelle Zahl   
+# R: reelle Zahl >= 1
+    
+def f_relu (x, R):
+    
+    return f_mult(f_id(x,R),sigmoid(R * x),R)
+
+# Neuronales Netz welches die Funktion f(x) = max(1 - (M/(2 * a)) * abs(x - y),0) approximiert
+#
+# x: reelle Zahl 
+# y: fixe reelle Zahl
+# R: reelle Zahl >= 1
+# M: natürliche Zahl
+# a: fixe Zahl > 0
+    
+def f_hat (x,y,R,M, a):
+    
+    return f_relu((M / (2 * a)) * (x - y) + 1) - 2 * f_relu((M / (2 * a)) * (x - y)) + 2 * f_relu((M / (2 * a)) * (x - y) - 1)
+    
