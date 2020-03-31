9fa08ea58ac7f4d43e8dca97e6960a956d915302
addi-g <56253562+addi-g@users.noreply.github.com>
2020-03-31 16:43:02 +0200

    kleinere Anpassungen

--- 01_Code/main.py
+++ 01_Code/main.py
@@ -21,105 +21,105 @@ n = 1000
 n_train = int(n * 0.8)
 n_test = int(n * 0.2)
 
-#'''
-#EINDIMENSIONALER FALL (d = 1) wird geplottet
-#'''
-#
-#N = 3
-#q = 2
-#R = 10 ** 6  
-#a = 2
-#M = 2
-#d = 1
-#
-#sigma = 0.05
-#
-## Parameter für unseren neuen Neuronale-Netze-Regressionschätzer
-#X_train = np.random.uniform(low=-2,high=2,size=(int(n_train),d))
-#m_X_train, Y_train = gen_data_Y(X_train,sigma)
-#    
-#X_test = np.random.uniform(low=-2,high=2,size=(int(n_test),d))
-#    
-#Y_pred_new_nn = new_neural_network_estimate(X_train, Y_train, X_test, N, q, R, d, M, a,)
-##Y_pred_fc_nn_1 = fc_neural_1_estimate(X_train, Y_train, X_test)
-##Y_pred_nearest_neighbor = nearest_neighbor_estimate(X_train, Y_train, X_test)
-#m_X_test, dummy = gen_data_Y(X_test,sigma)
-#    
-#
-##plt.plot(X_test, Y_pred_nearest_neighbor, '-r', label='nearest_neigbhor')
-##plt.plot(X_test, Y_pred_fc_nn_1, '-g', label='fc_nn_1')
-##colors = (0,0,0)
-#area = 4
-#plt.scatter(X_test, m_X_test, s=area, color = 'blue', label='m_1', alpha=0.5)
-#plt.scatter(X_test, Y_pred_new_nn, s=area, color = 'red', label='new_neural_network_estimate', alpha=0.5)
-#plt.title('...')
-#plt.legend(loc='upper left') 
-#plt.xlabel('x')
-#plt.ylabel('y')
-##plt.savefig('graph_d_1.png')
-#tikzplotlib.save("mytikz_d1.tex")
-##plt.show()
-##plt.plot(X_test, Y_pred_new_nn, 'ro-', label='new_nn')
-##plt.plot(X_test, m_X_test, '-b', label='m_d')   
-#
-##plt.xlim(-2, 2)
-##plt.xlim(-2,2)
-##plt.show()
-#
-#
-#'''
-#ZEIDIMENSIONALER FALL (d = 2) wird geplottet
-#'''
-#N = 2
-#q = 2
-#R = 10 ** 6  
-#a = 2
-#M = 2
-#d = 2
-#
-#sigma = 0.05
-#
-#
-## Parameter für unseren neuen Neuronale-Netze-Regressionschätzer
-#X_train = np.random.uniform(low=-2,high=2,size=(int(n_train),d))
-#m_X_train, Y_train = gen_data_Y(X_train,sigma)
-#    
-#X_test = np.random.uniform(low=-2,high=2,size=(int(n_test),d))
-#    
-#Y_pred_new_nn = new_neural_network_estimate(X_train, Y_train, X_test, N, q, R, d, M, a,)
-##Y_pred_fc_nn_1 = fc_neural_1_estimate(X_train, Y_train, X_test)
-##Y_pred_nearest_neighbor = nearest_neighbor_estimate(X_train, Y_train, X_test)
-#m_X_test, dummy = gen_data_Y(X_test,sigma)
-#    
-#
-#
-#x = np.ravel(X_test[:,0])
-#y = np.ravel(X_test[:,1])
-#
-## so wie es sein soll
-##z = m_X_test[:,0]
-## was der SChätzer auswirft
-#z_new = Y_pred_new_nn[:,0]
-#
-#ax = plt.axes(projection='3d')
-#ax.scatter(x, y, z_new, c=z_new, cmap='viridis', linewidth=0.5);
-#ax.view_init(40, 20)
-#plt.savefig('graph_d_2_new_estimate.png')
-#
-## so wie es sein soll
+'''
+EINDIMENSIONALER FALL (d = 1) wird geplottet
+'''
+
+N = 3
+q = 2
+R = 10 ** 6  
+a = 2
+M = 2
+d = 1
+
+sigma = 0.05
+
+# Parameter für unseren neuen Neuronale-Netze-Regressionschätzer
+X_train = np.random.uniform(low=-2,high=2,size=(int(n_train),d))
+m_X_train, Y_train = gen_data_Y(X_train,sigma)
+    
+X_test = np.random.uniform(low=-2,high=2,size=(int(n_test),d))
+    
+Y_pred_new_nn = new_neural_network_estimate(X_train, Y_train, X_test, N, q, R, d, M, a,)
+#Y_pred_fc_nn_1 = fc_neural_1_estimate(X_train, Y_train, X_test)
+#Y_pred_nearest_neighbor = nearest_neighbor_estimate(X_train, Y_train, X_test)
+m_X_test, dummy = gen_data_Y(X_test,sigma)
+    
+
+#plt.plot(X_test, Y_pred_nearest_neighbor, '-r', label='nearest_neigbhor')
+#plt.plot(X_test, Y_pred_fc_nn_1, '-g', label='fc_nn_1')
+#colors = (0,0,0)
+area = 4
+plt.scatter(X_test, m_X_test, s=area, color = 'blue', label='m_1', alpha=0.5)
+plt.scatter(X_test, Y_pred_new_nn, s=area, color = 'red', label='new_neural_network_estimate', alpha=0.5)
+plt.title('...')
+plt.legend(loc='upper left') 
+plt.xlabel('x')
+plt.ylabel('y')
+#plt.savefig('graph_d_1.png')
+tikzplotlib.save("mytikz_d1.tex")
+#plt.show()
+#plt.plot(X_test, Y_pred_new_nn, 'ro-', label='new_nn')
+#plt.plot(X_test, m_X_test, '-b', label='m_d')   
+
+#plt.xlim(-2, 2)
+#plt.xlim(-2,2)
+#plt.show()
+
+
+'''
+ZEIDIMENSIONALER FALL (d = 2) wird geplottet
+'''
+N = 2
+q = 2
+R = 10 ** 6  
+a = 2
+M = 2
+d = 2
+
+sigma = 0.05
+
+
+# Parameter für unseren neuen Neuronale-Netze-Regressionschätzer
+X_train = np.random.uniform(low=-2,high=2,size=(int(n_train),d))
+m_X_train, Y_train = gen_data_Y(X_train,sigma)
+    
+X_test = np.random.uniform(low=-2,high=2,size=(int(n_test),d))
+    
+Y_pred_new_nn = new_neural_network_estimate(X_train, Y_train, X_test, N, q, R, d, M, a,)
+#Y_pred_fc_nn_1 = fc_neural_1_estimate(X_train, Y_train, X_test)
+#Y_pred_nearest_neighbor = nearest_neighbor_estimate(X_train, Y_train, X_test)
+m_X_test, dummy = gen_data_Y(X_test,sigma)
+    
+
+
+x = np.ravel(X_test[:,0])
+y = np.ravel(X_test[:,1])
+
+# so wie es sein soll
 #z = m_X_test[:,0]
-## was der Schätzer auswirft
-#
-#ax = plt.axes(projection='3d')
-#ax.scatter(x, y, z, c=z, cmap='viridis', linewidth=0.5);
-#ax.view_init(40, 20)
-#plt.savefig('test.png')
-##tikzplotlib.save("mytikz_d2.tex")
-#
-#postpro = np.asarray([ X_test[:,0], X_test[:,1], Y_pred_new_nn[:,0] ])
-#np.savetxt("plotpostpro.csv", np.transpose(postpro), delimiter=",")
-#
-##plt.savefig('graph_d_2_m_2.png')
+# was der SChätzer auswirft
+z_new = Y_pred_new_nn[:,0]
+
+ax = plt.axes(projection='3d')
+ax.scatter(x, y, z_new, c=z_new, cmap='viridis', linewidth=0.5);
+ax.view_init(40, 20)
+plt.savefig('graph_d_2_new_estimate.png')
+
+# so wie es sein soll
+z = m_X_test[:,0]
+# was der Schätzer auswirft
+
+ax = plt.axes(projection='3d')
+ax.scatter(x, y, z, c=z, cmap='viridis', linewidth=0.5);
+ax.view_init(40, 20)
+plt.savefig('test.png')
+#tikzplotlib.save("mytikz_d2.tex")
+
+postpro = np.asarray([ X_test[:,0], X_test[:,1], Y_pred_new_nn[:,0] ])
+np.savetxt("plotpostpro.csv", np.transpose(postpro), delimiter=",")
+
+#plt.savefig('graph_d_2_m_2.png')
 
 '''
 ein Vergleich des emp. L2 Fehler gemacht für d = 1
@@ -188,7 +188,7 @@ for sigma in spreads:
         print("Der empirische L2 Fehler für d = 1 und sigma = 0.05 ist berechnet worden!")
         
     else:
-        series_noise_2 = pd.Series([repr(sigma)+'%',np.median(e_L2_avg),"(Median, IQR)",(median_new_nn, iqr_new_nn), (median_fc_nn_1, iqr_fc_nn_1), (median_nearest_neighbor, iqr_nearest_neighbor)], index=rows)
+        series_noise_2 = pd.Series([repr(sigma),np.median(e_L2_avg),"(Median, IQR)",(median_new_nn, iqr_new_nn), (median_fc_nn_1, iqr_fc_nn_1), (median_nearest_neighbor, iqr_nearest_neighbor)], index=rows)
         series_noise_2.name = ""
         print("Der empirische L2 Fehler für d = 1 und sigma = 0.1 ist berechnet worden!")
 
@@ -259,7 +259,7 @@ for sigma in spreads:
     rows = ["noise","e_L2_avg","approach","new_nn", "fc_nn_1", "nearest_neighbor"]
     
     if sigma == 0.05:
-        series_noise_1 = pd.Series([repr(sigma)+'%',np.median(e_L2_avg),"(Median, IQR)",(median_new_nn, iqr_new_nn), (median_fc_nn_1, iqr_fc_nn_1), (median_nearest_neighbor, iqr_nearest_neighbor)], index=rows)
+        series_noise_1 = pd.Series([repr(sigma),np.median(e_L2_avg),"(Median, IQR)",(median_new_nn, iqr_new_nn), (median_fc_nn_1, iqr_fc_nn_1), (median_nearest_neighbor, iqr_nearest_neighbor)], index=rows)
         series_noise_1.name = ""
         print("Der empirische L2 Fehler für d = 2 und sigma = 0.05 ist berechnet worden!")
 
